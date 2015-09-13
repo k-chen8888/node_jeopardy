3f68831510cb88ba3f17e4ee4c8c4ad83d58221d
Kevin Chen <k.chen8880@gmail.com>
2015-09-13 16:31:38 -0400

    Toast notification while waiting for players to join
    + jeopardy.js produces objects for the host and players
    + More form data
    Note: socket.io sends host and display the READY signal when n players
    have joined, where n was inputted in the form
    
    Cannot proceed without display done
    * Need to define makeGameBoard(data, clickable)

--- modules/game/jeopardy.js
+++ modules/game/jeopardy.js
@@ -5,14 +5,19 @@ module.exports = function(options) {
 	var exports = {};
 	
 	// General information for the game
-	exports.name = options.name || 'My Jeopardy Game'; // Name of the jeopardy game
+	exports.name = options.gameName || 'My Jeopardy Game'; // Name of the jeopardy game
 	exports.data = options.data || '/sample';		   // Location of game pages (relative to location of index.js)
 	exports.numPlayers = options.numPlayers || 2;	   // Number of players
 	
+	// Host identification
+	exports.host = {
+		name: options.hostName || 'Alex Trebec',
+		id: Math.floor(Math.random() * (Math.pow(32, 4) - 1 - Math.pow(32, 3)) + Math.pow(32, 3)).toString(32);
+	};
+	
 	// Player identification
-	exports.host = Math.floor(Math.random() * (Math.pow(32, 4) - 1 - Math.pow(32, 3)) + Math.pow(32, 3)).toString(32);
 	exports.players = [];
-	for(var i = 0; i < exports.numPlayers; i++) exports.players.push( Math.floor(Math.random() * (Math.pow(32, 4) - 1 - Math.pow(32, 3)) + Math.pow(32, 3)).toString(32) );
+	for(var i = 0; i < exports.numPlayers; i++) exports.players.push( {id: Math.floor(Math.random() * (Math.pow(32, 4) - 1 - Math.pow(32, 3)) + Math.pow(32, 3)).toString(32)} );
 	
 	return exports;
 };
--- pages/js/host.js
+++ pages/js/host.js
@@ -1,9 +1,5 @@
 /* Global variables */
 
-// Info variables
-var host_id,
-	players;
-
 // URL parameters
 var params;
 
@@ -21,14 +17,54 @@ var init = function() {
 	params = getUrlParam();
 	
 	// Display content based on URL prarmeters
-	if (params['id'] && params['room']) {
-		// Display host interface, which is just a clickable version of the display
+	if (params['room'] && params['host']) {
+		// Note to host: DO NOT REVEAL YOUR ID!!
 		
+		// Makes it look like there's something going on, in case players take forever
+		toasts.push(makeToast({
+			classes: 'waiting',
+			info: {
+				// Parameters that the function can actually use
+				content: '<p id="content">Waiting for players to join...</p>',
+			},
+			
+			// Deleted when socket.io says so
+			edible = false
+		}));
+		
+		// Wait for all players to join
+		socket.on('gameready', function(data) {
+			// Replace the toast, then delete it after 2 seconds
+			toasts[0].obj.removeClass('waiting');
+			toasts[0].obj.addClass('ready');
+			toasts[0].obj.children('p#content').text('Ready!');
+			setTimeout(function(){
+				eatToast(toasts[0]);
+				toasts = [];
+			}, 2000);
+			
+			// Display host interface, which is just a clickable version of the display
+			makeGameBoard(data, true);
+		});
 	} else {
 		// Ask host to create a room
-		$('body').append('<button id="makegame">New Jeopardy Game</button>');
+		$('body').append(
+			'<form id="makegame">' +
+				'Host\'s Name:<br>' +
+				'<input type="text" name="hostname"><br><br>' +
+				'Game Name:<br>' +
+				'<input type="text" name="gamename"><br><br>' +
+				'Number of Players (2-8):<br>' +
+				'<input type="number" name="numplayers" min="2" max="8"><br><br>' +
+				'<button id="makegame">New Jeopardy Game</button>' +
+			'</form>'
+		);
 		$('button#makegame').click(function() {
-			socket.emit('makegame', {});
+			socket.emit('makegame', {
+				hostName: $('input[name=hostname]').val(),
+				gameName: $('input[name=gamename]').val(),
+				numPlayers: $('input[name=numplayers]').val()
+			});
 		});
 		
 		// Set up a listener that redirects the host to the game when it's ready
