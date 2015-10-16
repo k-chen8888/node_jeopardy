a8ed295fd48186e6263247a56069ae6d26cd9fc5
Kevin Chen <k.chen8880@gmail.com>
2015-10-16 15:35:45 -0400

    Server-side display data handler

--- pages/js/display.js
+++ pages/js/display.js
@@ -4,61 +4,86 @@
 var params;
 
 // socket.io
-var socket = io();
+var client = io();
 
 // Toasts
 var toasts = [];
 
 
-/* Declares no connection at 5 seconds */
-var no_conn = setTimeout(function() {
-	// Display failed to connect, update toast accordingly
-	toasts[0].obj.removeClass('waiting');
-	toasts[0].obj.addClass('display failed');
-	toasts[0].obj.children('p#content').text('Failed to connect');
-}, 5000);
-
-
 /* Initialization function */
 var init = function() {
-	// Until data from socket.io is received, put up toast
-	toasts.push(makeToast({
-		classes: 'display waiting',
-		info: {
-			content: '<p id="content">Waiting for data...</p>'
-		},
-		edible: false
-	}));
-	
-	// Receive data from socket.io
-	/*socket.on('displaydata', function(data) {
-		// Replace the toast, then delete it after 2 seconds
-		toasts[0].obj.removeClass();
-		toasts[0].obj.addClass('toast display ready');
-		toasts[0].obj.children('p#content').text('Ready!');
-		setTimeout(function(){
-			eatToast(toasts[0]);
-			toasts = [];
-		}, 2000);
+	return new Promise(function(resolve, reject) {
+		// Until data from socket.io is received, put up toast
+		toasts.push(makeToast({
+			classes: 'display waiting',
+			info: {
+				content: '<p id="content">Waiting for data...</p>'
+			},
+			edible: false
+		}));
 		
-		// Draw scorecard
-		makeScoreCard(data.players);
+		client.on('connect', function() {
+			resolve(true);
+		});
 		
-		// Draw display interface, non-clickable objects
+		client.on('connect_error', function(err) {
+			// Display failed to connect, update toast accordingly
+			toasts[0].obj.removeClass();
+			toasts[0].obj.addClass('toast display failed');
+			toasts[0].obj.children('p#content').text('Failed to connect');
+			
+			reject(err);
+		});
+	}).then(function(cont) {
+		params = getUrlParams();
+		
+		if (params.length > 2 && params['room']) {
+			// Render game display
+			client.emit('add_disp', {room: param['room']});
+			
+			// Receive data from socket.io
+			client.on('display_data', function(data) {
+				// Replace the toast, then delete it after 2 seconds
+				toasts[0].obj.removeClass();
+				toasts[0].obj.addClass('toast display ready');
+				toasts[0].obj.children('p#content').text('Ready!');
+				setTimeout(function() {
+					eatToast(toasts[0]);
+					toasts = [];
+				}, 2000);
+				
+				// Draw scorecard
+				makeScoreCard(data.players);
+				
+				// Draw display interface, non-clickable objects
+				showPlayers(data.playerInfo);
+				showBoard(data.board, false);
+				
+				// Resizes everything when the game starts and whenever the screen size changes
+				renderJeopardy();
+				renderPlayers();
+				$(window).resize(function() {
+					renderJeopardy();
+					renderPlayers();
+				});
+			});
+		} else {
+			// Continue waiting
+		}
+	}, function(err) {
+		console.log('Connection Error');
+		
+		// Test drawing, base styles off of this
+		showBoard(testdata, false);
 		showPlayers(testPlayerInfo);
-		showBoard(data.board, false);
-	});*/
-	
-	// Test drawing, base styles off of this
-	showBoard(testdata, false);
-	showPlayers(testPlayerInfo);
-	
-	// Resizes everything when the game starts and whenever the screen size changes
-	renderJeopardy();
-	renderPlayers();
-	$(window).resize(function() {
+		
+		// Resizes everything when the game starts and whenever the screen size changes
 		renderJeopardy();
 		renderPlayers();
+		$(window).resize(function() {
+			renderJeopardy();
+			renderPlayers();
+		});
 	});
 };
 
